eed952f830399ea5fd85a764a50d417031a74d2e
Sabrina <sab.shine.art@gmail.com>
2025-02-28 14:44:10 +0100

    Working on an improved Stanley controller

--- Program.cs
+++ Program.cs
@@ -13,14 +13,14 @@ namespace PathTrackingSimulation
             Application.SetCompatibleTextRenderingDefault(false);
 
             //CONTROLLER: Baseline, Stanley or PurePursuit
-            ControllerType selectedController = ControllerType.Baseline;
-            // ControllerType selectedController = ControllerType.Stanley;
+            // ControllerType selectedController = ControllerType.Baseline;
+            ControllerType selectedController = ControllerType.Stanley;
             // ControllerType selectedController = ControllerType.PurePursuit;
 
             //PATH SHAPE: straight, curved or zigzag
             // PointF[] selectedPath = PathGenerator.CreateStraightPath(new PointF(50, 300), new PointF(800, 300));
-            PointF[] selectedPath = PathGenerator.CreateCurvedPath(new PointF(50, 300), new PointF(800, 300));
-            // PointF[] selectedPath = PathGenerator.GenerateZigzagPath(750, 80, 50);
+            // PointF[] selectedPath = PathGenerator.CreateCurvedPath(new PointF(50, 300), new PointF(800, 300));
+            PointF[] selectedPath = PathGenerator.GenerateZigzagPath(750, 80, 50);
 
             //SET SPEED
             float selectedSpeed = 2.0f; //1.0f is 50 px/s
