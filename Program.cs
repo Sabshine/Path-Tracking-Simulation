19d744b8880776db651c91f47a63a21aa65219f6
Sabrina <sab.shine.art@gmail.com>
2025-02-28 16:39:53 +0100

    Adjusted text size

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ namespace PathTrackingSimulation
             Application.SetCompatibleTextRenderingDefault(false);
 
             //CONTROLLER: Baseline, Stanley or PurePursuit
-            // ControllerType selectedController = ControllerType.Baseline;
+            ControllerType selectedController = ControllerType.Baseline;
             // ControllerType selectedController = ControllerType.Stanley;
-            ControllerType selectedController = ControllerType.PurePursuit;
+            // ControllerType selectedController = ControllerType.PurePursuit;
 
             //PATH SHAPE: straight, curved or zigzag
             PointF[] selectedPath = PathGenerator.CreateStraightPath(new PointF(50, 300), new PointF(800, 300));
@@ -23,7 +23,7 @@ namespace PathTrackingSimulation
             // PointF[] selectedPath = PathGenerator.GenerateZigzagPath(750, 80, 50);
 
             //SET SPEED
-            float selectedSpeed = 2.0f; //1.0f is 50 px/s
+            float selectedSpeed = 0.5f; //1.0f is 50 px/s
 
             //Start application
             Application.Run(new PathTrackingForm(selectedController, selectedPath, selectedSpeed));
